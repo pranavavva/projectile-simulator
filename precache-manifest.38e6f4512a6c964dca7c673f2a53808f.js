beb124bde72991f149b05e7154a88ea4dd53d97a
Pranav Avva <pranav.avva@gmail.com>
2019-05-22 22:47:19 -0400

    Updates

---
+++ precache-manifest.38e6f4512a6c964dca7c673f2a53808f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d802cad06052c10760ee499e8ae866",
+    "url": "/projectile-simulator/index.html"
+  },
+  {
+    "revision": "9df15893d7ea5886ebb5",
+    "url": "/projectile-simulator/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2fb791c262df556ade7d",
+    "url": "/projectile-simulator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9df15893d7ea5886ebb5",
+    "url": "/projectile-simulator/static/js/2.44b2ef5f.chunk.js"
+  },
+  {
+    "revision": "2fb791c262df556ade7d",
+    "url": "/projectile-simulator/static/js/main.d1ad32f6.chunk.js"
+  },
+  {
+    "revision": "e0c1ba1240c2079428d1",
+    "url": "/projectile-simulator/static/js/runtime~main.dd814bdc.js"
+  }
+]);
